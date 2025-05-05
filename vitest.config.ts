32cc9c669673849b418b82958ff29c54aa4bb492
David Higgins <thehig+github@gmail.com>
2025-05-05 12:33:25 +0100

    chore: Enhance vitest configuration for improved test coverage
    - Added workspace and exclude patterns to vitest configuration for better test management.
    - Updated coverage settings to exclude additional files and directories, ensuring cleaner reports.

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,28 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    workspace: ["packages/*"],
+    exclude: [
+      "node_modules",
+      "dist",
+      "build",
+      "coverage",
+      "**/DO_NOT_EDIT_THIS/**",
+    ],
     coverage: {
       provider: "v8", // or 'istanbul'
       reporter: ["text", "json", "html"],
       reportsDirectory: "./coverage", // Output directory for the combined report
+
+      exclude: [
+        "node_modules",
+        "dist",
+        "build",
+        "coverage",
+        "**/DO_NOT_EDIT_THIS/**",
+        "vitest.*",
+        "**/*.config.*",
+      ],
     },
   },
 });
