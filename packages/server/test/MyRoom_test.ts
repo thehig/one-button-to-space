a2c7ac034f24a982160633c6296e12e3d19de87f
David Higgins <thehig+github@gmail.com>
2025-05-16 17:30:30 +0100

    chore(dependencies): update package versions and improve test setup
    - Bumped versions of concurrently, parcel, and type definitions in package.json files.
    - Updated pnpm workspace to use wildcard versions for Colyseus and related packages.
    - Enhanced MyRoom_test.ts to improve state validation and added player position checks.

--- packages/server/test/MyRoom_test.ts
+++ packages/server/test/MyRoom_test.ts
@@ -1,7 +1,7 @@
-import assert from "assert";
 import { ColyseusTestServer, boot } from "@colyseus/testing";
+import { describe, it, before, after, beforeEach } from "mocha";
+import * as assert from "assert";
 
-// import your "arena.config.ts" file here.
 import appConfig from "../src/app.config";
 import { MyRoomState } from "../src/rooms/Part1Room";
 
@@ -15,7 +15,7 @@ describe("testing your Colyseus app", () => {
 
   it("connecting into a room", async () => {
     // `room` is the server-side Room instance reference.
-    const room = await colyseus.createRoom<MyRoomState>("my_room", {});
+    const room = await colyseus.createRoom<MyRoomState>("part1_room", {});
 
     // `client1` is the client-side `Room` instance reference (same as JavaScript SDK)
     const client1 = await colyseus.connectTo(room);
@@ -26,9 +26,36 @@ describe("testing your Colyseus app", () => {
     // wait for state sync
     await room.waitForNextPatch();
 
-    assert.deepStrictEqual(
-      { mySynchronizedProperty: "Hello world" },
-      client1.state.toJSON()
+    const actualState = client1.state.toJSON();
+
+    assert.partialDeepStrictEqual(actualState, {
+      mapHeight: 600,
+      mapWidth: 800,
+      players: {
+        [client1.sessionId]: {
+          // x: assert.within
+          // y: 509.3070373535156,
+        },
+      },
+    });
+
+    assert.equal(
+      actualState.players[client1.sessionId].x > 0 &&
+        actualState.players[client1.sessionId].x < actualState.mapWidth,
+      true
     );
+
+    assert.equal(
+      actualState.players[client1.sessionId].y > 0 &&
+        actualState.players[client1.sessionId].y < actualState.mapHeight,
+      true
+    );
+
+    client1.send(0, {
+      x: 100,
+      y: 100,
+    });
+
+    await room.waitForNextPatch();
   });
 });
