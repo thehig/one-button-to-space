992809bfe333b72f229bbd31c92d037f78cbc9e7
David Higgins <thehig+github@gmail.com>
2025-05-16 18:11:29 +0100

    refactor(app.config.ts): streamline game server and express initialization
    - Consolidated room definitions and express route bindings into separate modules for improved organization.
    - Replaced individual room imports with dynamic loading from `./rooms`.
    - Enhanced express route setup by importing routes from `./routes/routes`.

---
+++ packages/server/src/routes/routes.ts
@@ -0,0 +1,34 @@
+import { Server } from "@colyseus/core";
+import { monitor } from "@colyseus/monitor";
+import { playground } from "@colyseus/playground";
+
+import { Request, Response } from "express";
+
+let gameServerRef: Server;
+let latencySimulationMs: number = 0;
+
+const routes = (_gameServerRef: Server) => {
+  gameServerRef = _gameServerRef;
+
+  return {
+    "/hello": (req: Request, res: Response) => {
+      res.send("It's time to kick ass and chew bubblegum!");
+    },
+    "/latency": (req: Request, res: Response) => {
+      res.json(latencySimulationMs);
+    },
+    "/simulate-latency/:milliseconds": (req: Request, res: Response) => {
+      latencySimulationMs = parseInt(req.params.milliseconds || "100");
+
+      // enable latency simulation
+      gameServerRef.simulateLatency(latencySimulationMs);
+
+      res.json({ success: true });
+    },
+
+    "/": playground(),
+    "/colyseus": monitor(),
+  };
+};
+
+export default routes;
